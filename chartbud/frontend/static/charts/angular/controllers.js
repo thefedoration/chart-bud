334da0fd21f15fdc0bbc37d644f43ce54af7a123
Fedor Garin <fedor.garin@gmail.com>
2017-12-30 23:14:19 -0500

    some basic chart getting involved

--- chartbud/frontend/static/charts/angular/controllers.js
+++ chartbud/frontend/static/charts/angular/controllers.js
@@ -198,6 +198,7 @@ chartsControllers.controller('stockCtrl', ['$rootScope', '$scope', '$state', '$t
             .success(function(data, status, headers, config) {
                 $scope.loadingChart = false;
                 $scope.chartData = data;
+                $scope.drawChart();
             })
             .error(function(data, status, headers, config) {
                 // -1 status means cancelled
@@ -207,6 +208,95 @@ chartsControllers.controller('stockCtrl', ['$rootScope', '$scope', '$state', '$t
                 }
             });
         }
+        
+        // draws chart based on data
+        $scope.drawChart = function(){
+            var config = {
+                type: 'line',
+                data: {
+                    labels: ["January", "February", "March", "April", "May", "June", "July"],
+                    datasets: [{
+                        label: "Unfilled",
+                        fill: false,
+                        backgroundColor: window.chartColors.blue,
+                        borderColor: window.chartColors.blue,
+                        data: [
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor()
+                        ],
+                    }, {
+                        label: "Dashed",
+                        fill: false,
+                        backgroundColor: window.chartColors.green,
+                        borderColor: window.chartColors.green,
+                        borderDash: [5, 5],
+                        data: [
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor()
+                        ],
+                    }, {
+                        label: "Filled",
+                        backgroundColor: window.chartColors.red,
+                        borderColor: window.chartColors.red,
+                        data: [
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor(),
+                            randomScalingFactor()
+                        ],
+                        fill: true,
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    title:{
+                        display:true,
+                        text:'Chart.js Line Chart'
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Month'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Value'
+                            }
+                        }]
+                    }
+                }
+            };
+
+            var ctx = document.getElementById("chart-canvas").getContext("2d");
+            window.myLine = new Chart(ctx, config);
+        }
+        
         
         // ACTIONS
         // /////////////////////
--- chartbud/stocks/models.py
+++ chartbud/stocks/models.py
@@ -144,7 +144,7 @@ class TimeseriesResult(BaseModel):
 
     # determines if a result is stale based on when it was last updated
     def _result_is_stale(self):
-        if self.datetime_updated:
+        if self.datetime_updated and self.result:
             if self.time_period == "current" and self.datetime_updated > timezone.now() - datetime.timedelta(minutes=1):
                 return False
             if self.time_period == "1d" and self.datetime_updated > timezone.now() - datetime.timedelta(minutes=1):
